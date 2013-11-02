3ac4c7ba97df127606048932ce041d352e57ee2a
Nicole <nicole.cadi@gmail.com>
2013-11-02 19:05:59 +0000

    Added 1- and 2-word feature method sigs

--- src/typeClassification/FeatureGenerator.java
+++ src/typeClassification/FeatureGenerator.java
@@ -16,15 +16,16 @@ import java.util.List;
  */
 public class FeatureGenerator {
 
+    List<Feature> featureList = new ArrayList<Feature>();
     String[] punctuation = {".",",","\'","\"","-","/","\\","(",")"};
 
     //returns list of features
     public List<Feature> genFeatures(Tree parse, Collection typedDependencies){
 
-        List<Feature> featureList = new ArrayList<Feature>();
+        List<Word> words = parse.yieldWords();
 
         //Sentence length
-        featureList.add(new Feature("sentence length", getSentenceLength(parse)));
+        featureList.add(new Feature("sentence length", getSentenceLength(words)));
 
         System.out.println("## Features: ##");
         for (Feature f : featureList){
@@ -38,21 +39,36 @@ public class FeatureGenerator {
     /**
      * Returns the length of the sentence represented by the given parse tree.
      *
-     * @param parse     Parse tree of relevant sentence.
+     * @param words     The words making up the given sentence.
      * @return          The sentence length.
      */
-    private int getSentenceLength(Tree parse){
-        List<Word> words = parse.yieldWords();
+    private int getSentenceLength(List<Word> words){
         int size = words.size();
-
         for (Word w : words){
             boolean isPunctuation = Arrays.asList(punctuation).contains(w.value().trim());
             if (isPunctuation){
                 size -= 1;
             }
         }
-
         return size;
+    }
+
+    /**
+     * Generates single-word features for the given sentence.
+     *
+     * @param words     Words making up given sentence.
+     */
+    private void getSingleWordFeatures(List<Word> words){
+
+    }
+
+    /**
+     * Generates two-word features for the given sentence.
+     *
+     * @param words     Words making up given sentence.
+     */
+    private void getWordPairFeatures(List<Word> words){
+
     }
 
 }
