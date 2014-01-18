d74e93d60dc42599ac1a6c8b7fab47749c0c20d6
Nicole <nicole.cadi@gmail.com>
2014-01-18 21:08:33 +0000

    Extracting NSUs: parsing NSU list

--- src/dataExtraction/NSUExtractor.java
+++ src/dataExtraction/NSUExtractor.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.charset.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,18 +21,25 @@ public class NSUExtractor {
 
     Path nsurf = Paths.get("C:\\DataFiles\\Programming\\4th Year Project - Ellipsis Interpretation\\NSU-RF");
 
-
+    List<SentenceReference> sentenceRefs = new ArrayList<SentenceReference>();
 
     public void extractNSUs(){
 
         try {
             BufferedReader reader = Files.newBufferedReader(nsurf, charset);
             String line;
+
+            //create a list of sentence references from the NSU-RF file
             while ((line = reader.readLine()) != null) {
+                String[] lineSplit = line.split(" ");
 
+                String text = lineSplit[0];
+                int antecedent = Integer.parseInt(lineSplit[1]);
+                int nsu = Integer.parseInt(lineSplit[2]);
+                String category = lineSplit[3];
 
-
-
+                SentenceReference sref = new SentenceReference(text, antecedent, nsu, category);
+                sentenceRefs.add(sref);
             }
         } catch (IOException x) {
             System.err.format("IOException: %s%n", x);
--- src/dataExtraction/SentenceReference.java
+++ src/dataExtraction/SentenceReference.java
@@ -7,4 +7,20 @@ package dataExtraction;
  *         18/01/14
  */
 public class SentenceReference {
+
+    String text;
+
+    //sentence IDs within the text
+    int antecedent;
+    int nsu;
+
+    //according to the NSU taxonomy due to Fernandez, Ginsburg & Lappin
+    String nsuCategory;
+
+    public SentenceReference(String text, int antecedent, int nsu, String nsuCategory){
+        this.text = text;
+        this.antecedent = antecedent;
+        this.nsu = nsu;
+        this.nsuCategory = nsuCategory;
+    }
 }
