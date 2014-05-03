17af8900c6b2d931c956d153c6602340bb12d9c1
Nicole <nicole.cadi@gmail.com>
2014-05-03 19:10:53 +0100

    Set up structure of interpretation

--- src/ellipsisInterpretation/EllipsisInterpreter.java
+++ src/ellipsisInterpretation/EllipsisInterpreter.java
@@ -1,5 +1,14 @@
 package ellipsisInterpretation;
 
+import edu.stanford.nlp.ling.Label;
+import edu.stanford.nlp.trees.Constituent;
+import edu.stanford.nlp.trees.Tree;
+import ellipsisDetection.EllipsisType;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -10,7 +19,54 @@ package ellipsisInterpretation;
  */
 public class EllipsisInterpreter {
 
+    /**
+     * Given the parsed representation of a sentence and the type of ellipsis detected in it, provide an antecedent.
+     *
+     * @param parse                     Parse of sentence containing ellipsis
+     * @param typedDependencies         Typed dependencies of same sentence
+     * @param ellipsisType              Type of ellipsis detected in that sentence
+     * @return                          An antecedent for the elliptical construction
+     */
+    public String interpretEllipsis(Tree parse, Collection typedDependencies, EllipsisType ellipsisType){
 
+        switch (ellipsisType){
+            case NPE:
+                return resolveNPE(parse, typedDependencies);
+            case VPE:
+                return resolveVPE(parse, typedDependencies);
+            case NSU:
+                //TODO: content
+                break;
+            case GAPPING:
+                //TODO: content
+                break;
+            default:
+                //TODO: content
+                break;
+        }
+
+
+        return null;        //TODO: default return statement.
+    }
+
+    /**
+     * Return an antecedent for a sentence known to contain noun phrase ellipsis
+     */
+    private String resolveNPE(Tree parse, Collection typedDependencies){
+
+        Iterator iterator = parse.iterator();
+
+        while (iterator.hasNext()){
+            Tree next = (Tree) iterator.next();
+            System.out.println(next.label());       //TODO: acquire NP subtrees
+        }
+
+        return null;        //TODO: default return statement.
+    }
+
+    private String resolveVPE(Tree parse, Collection typedDependencies){
+        return null;        //TODO: default return statement.
+    }
 
 
 }
