f645fe7fd3121fac1cc0d2ea3f82b995fcb77212
Nicole <nicole.cadi@gmail.com>
2014-05-03 17:18:54 +0100

    Added interpreter class

---
+++ src/ellipsisInterpretation/EllipsisInterpreter.java
@@ -0,0 +1,16 @@
+package ellipsisInterpretation;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author Nicole Williams
+ *         03/05/14
+ *
+ * Class to handle finding an antecedent for an instance of ellipsis in a given sentence.
+ */
+public class EllipsisInterpreter {
+
+
+
+
+}
