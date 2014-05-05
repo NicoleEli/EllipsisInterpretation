f1ad725ec5cd7e28fea6d368bf59e32863c35819
Nicole <nicole.cadi@gmail.com>
2014-05-05 17:16:15 +0100

    Interpretation evaluation creator

---
+++ src/testing/InterpretationAsessment.java
@@ -0,0 +1,17 @@
+package testing;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author Nicole Williams
+ *         05/05/14
+ *
+ * Prepares assessment sheets for manual evaluation of ellipsis interpreter.
+ */
+public class InterpretationAsessment {
+
+    public InterpretationAsessment(){
+        //TODO: content
+    }
+
+}
